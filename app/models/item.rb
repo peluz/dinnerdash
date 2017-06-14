2f237412b2fcfc9695c5adaaae14aef5fcfb3028
peluz <pedrohluzaraujo@gmail.com>
2017-06-13 21:31:25 -0300

    user story 1 em review

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+	has_and_belongs_to_many :category
+	validates :title, presence: true, uniqueness: true
+	validates :description, presence: true
+	validates_numericality_of :price, :greater_than_or_equal_to => 0.01
 end
