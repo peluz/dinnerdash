72143ad28a410bd65a5a062337a1d2f468d97ce7
peluz <caezarluz@hotmail.com>
2017-06-20 13:49:50 -0300

    pode adicionar item carrinho

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,11 @@
 class CartsController < ApplicationController
+	def index
+		
+	end
+
+	def add
+		@items = Item.all	#dps do render view do carriho index pode retirar
+		current_cart << {id: params[:item_id], quantity: 1}
+		render 'items/index' #render pro view do carrinho index
+	end
 end
