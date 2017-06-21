e6e070071b35a5cd85852a96055159368198a809
Lucas Moutinho <lucasmoutinho>
2017-06-21 18:00:00 -0300

    doing some changes at destroy cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -7,16 +7,21 @@ class CartsController < ApplicationController
 	def add
 		@items = Item.all	#dps do render view do carriho index pode retirar
 		current_cart << {id: params[:item_id], quantity: 1}
-		@cart = current_cart
-		merged_cart = current_cart.group_by {|h1| h1[:id]}.map do |k,v|
-			{:id => k, :quantity => v.map { |h2| h2[:quantity]}.sum}
-		end
-		@cart = merged_cart
+		@cart = merge_cart
 		render 'carts/index' #render pro view do carrinho index
 	end
 
 	def destroy
-		@cart = merged_cart
-		byebug
+		@items = Item.all
+		@cart = current_cart
+		render 'index'
 	end
+
+	private
+
+		def merge_cart
+			merged_cart = current_cart.group_by {|h1| h1[:id]}.map do |k,v|
+				{:id => k, :quantity => v.map { |h2| h2[:quantity]}.sum}
+				end		
+		end
 end
