a8767137f8e90dd5010445ecac208b1aa03637f8
Lucas Moutinho <lucasmoutinho>
2017-06-27 13:54:32 -0300

    new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(params[user_params])
+		if @user.save
+			redirect_to items_path
+		else
+			render 'new'
+		end
+	end
+
+	private
+		def user_params
+			params.require(:user).permit(:username, :email, :password, :password_confirmation, :address)
+		end
 end
