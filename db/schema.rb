f0e5cffcda798c76d4175b04f4c640810176a8b8
peluz <caezarluz@hotmail.com>
2017-07-01 16:48:34 -0300

    upgrade view carrinho

--- db/schema.rb
+++ db/schema.rb
@@ -24,8 +24,6 @@ ActiveRecord::Schema.define(version: 20170627161520) do
   create_table "categories_items", id: :bigint, default: nil, force: :cascade do |t|
     t.bigint "category_id", null: false
     t.bigint "item_id", null: false
-    t.index ["category_id", "item_id"], name: "index_categories_items_on_category_id_and_item_id"
-    t.index ["item_id", "category_id"], name: "index_categories_items_on_item_id_and_category_id"
   end
 
   create_table "items", force: :cascade do |t|
