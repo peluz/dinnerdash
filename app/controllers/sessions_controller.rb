ba3d69e783b8e338568eb9afda40368877f641e3
Lucas Moutinho <lucasmoutinho>
2017-07-01 16:11:25 -0300

    Add log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,5 +13,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,4 @@ Rails.application.routes.draw do
   get    '/login' => 'sessions#new'
   post   '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
-
 end
