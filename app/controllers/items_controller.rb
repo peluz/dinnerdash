e1bfc0331410449e9dafde0df0a9f2517bd97c1b
Joao Gabriel <joaoneves@cjr.org.br>
2017-07-11 17:03:44 -0300

    develop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,16 @@ class ItemsController < ApplicationController
   def show
   	@item = Item.find(params[:id])
   end
+
+  def create
+  	@item = Item.new(item_params)
+  	if @item.save
+  		redirect_to @item
+  	else
+  		render 'new'
+  	end		
+  end
+  def new
+  	@item = Item.new
+  end	
 end
