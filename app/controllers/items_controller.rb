9a8b7c21be4238017db8afa1b3d7e998c4fbbacd
peluz <caezarluz@hotmail.com>
2017-07-12 11:06:42 -0300

    fix show item for signed users

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :check_admin, :only => [:edit, :destroy, :update, :create, :new, :show]
+  before_action :check_admin, :only => [:edit, :destroy, :update, :create, :new]
   def index
   	@items = Item.all	
   end
