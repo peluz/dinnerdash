b604b2170a73a06fb924bc3cbbedb541fc87937e
Joao Gabriel <joaoneves@cjr.org.br>
2017-07-11 23:29:16 -0300

    creat categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,5 +5,20 @@ class CategoriesController < ApplicationController
 
   def show
 	@category = Category.find(params[:id])
-	end
+  end
+  def create
+  	@categories = Category.new(categories_params)
+  	if @categories.save
+  		redirect_to @categories
+  	else
+  		render 'new'
+  	end		
+  end
+  def new
+  	@categories = Category.new
+  end
+  private
+  	def categories_params
+  		params.require(:category).permit(:title)
+  	end	
 end
